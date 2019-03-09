9e0c8933d65f92fa65faf29c67c469d878db007c
elirud <eli.rud@hotmail.com>
2019-03-09 19:41:31 +0100

    Adds components to start working from

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { PlayerComponent } from './player/player.component';
+import { HeroComponent } from './hero/hero.component';
+import { MatchComponent } from './match/match.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    PlayerComponent,
+    HeroComponent,
+    MatchComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
